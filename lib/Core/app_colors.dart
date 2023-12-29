56096a58a7a9915d0e27c4a6811a5ca654aef6b6
bernadinkele <kelebernadin00@gmail.com>
2023-12-29 00:38:22 +0000

    add app config

---
+++ lib/Core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Pallete{
+  static const Color primaryColor = Color(0xFF0560FD);
+  static const Color textColor = Color(0xFF000000);
+  static const Color borderColor = Color(0xFFD4D4D4);
+  static const Color placeHolderBacColor = Color(0xFFF6F6F6);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+}
